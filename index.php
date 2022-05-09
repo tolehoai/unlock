8dfde43338474366722fc874a554a4e0f4f4f19e
tolehoai <tolehoai@gmail.com>
2022-05-09 16:55:37 +0700

    feature: add Trait

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 
     require 'vendor/autoload.php';
-
     use Tolehoai\Unlock\Application;
+
     error_reporting(E_ALL);
     ini_set('display_errors', '1');
+
     $application = new Application();
-    echo "Hello";
+    echo $application->tinhTong(4,5);
