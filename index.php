4c57bcc7cf6ed66001ac358ee35270f67418cc2e
tolehoai <tolehoai@gmail.com>
2022-05-09 15:03:29 +0700

    feature: add Controller

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 
-    echo ("Hello World");
+    require 'vendor/autoload.php';
+
+    use Tolehoai\Unlock\Application;
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
+    $application = new Application();
+    echo "Hello";
---
+++ src/Application.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tolehoai\Unlock;
+
+
+class Application
+{
+
+    public function __construct()
+    {
+       echo "Hello";
+    }
+
+
+}
