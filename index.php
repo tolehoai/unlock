b4469e45bd81d1b3010bef2c7e43c952032d2c91
tolehoai <tolehoai@gmail.com>
2022-05-12 17:48:53 +0700

    fix/remove-debug-code

--- index.php
+++ index.php
@@ -7,19 +7,13 @@ use Tolehoai\Mvc\decorator\Sugar;
 
 
 require 'vendor/autoload.php';
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
 
-//$milkTea = new Sugar(new Bubble(new MilkTea())) ;
+
 $milkTea = new Sugar(new Bubble(new MilkTea())) ;
 echo "Name: ". $milkTea->getName();
 echo "<br/>";
 echo "Price: ".$milkTea->getPrice() ." VND";
-//$milkTea = new MilkTea();
-//$milkTea->showDrinkInfomation();
-//
-//$milkTeaBlackBubble = new MilkTeaBlackBubble();
-//$milkTeaBlackBubble->showDrinkInfomation();
+
 
 
 
